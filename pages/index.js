87951da1454540300effe90e82a6314a6b851543
Ivan Pravdin <72220773+ivanpravdin@users.noreply.github.com>
2022-03-14 14:10:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css">
+      <link href="css/singlePageTemplate.css" rel="stylesheet" type="text/css"></link>
       <title>IV</title>
    </head>
    <div id="wrapper">
