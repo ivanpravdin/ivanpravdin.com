c5923965dbf71a128446eb5b97e57740f893c429
Ivan Pravdin <72220773+ivanpravdin@users.noreply.github.com>
2022-03-14 18:15:31 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
                   <h3>3D Collages</h3>
                   <div class="row">
                      <div class="column">
-                        <img src={Image}  style={{ width: '100%' }} />
+                        <Image src={Image}  style={{ width: '100%' }} />
                      </div>
                      <div class="column">
                         
