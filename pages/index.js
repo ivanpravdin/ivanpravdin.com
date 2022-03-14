b3eb70ecd3e16af26e5bfaa5c8dcfed81e035cf5
Ivan Pravdin <72220773+ivanpravdin@users.noreply.github.com>
2022-03-14 17:41:09 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
                   <h3>3D Collages</h3>
                   <div class="row">
                      <div class="column">
-                        <img src={require('../images/3dcollages1.jpg')}  style={{ width: '100%' }} />
+                        <img src={require("../images/background.jpg")}  style={{ width: '100%' }} />
                      </div>
                      <div class="column">
                         
