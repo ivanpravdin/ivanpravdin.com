193978d67365d6d02dbcdb774fb95a38f5a4a955
Ivan Pravdin <72220773+ivanpravdin@users.noreply.github.com>
2022-03-14 17:24:58 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import collages3d1 from '@images/3dcollages1.jpg'
+import collages3d1 from '/images/3dcollages1.jpg'
 
 export default function Home() {
     return (
