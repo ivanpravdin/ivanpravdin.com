c7aa3ad35ffa4267c7dd71787274feeddb9a3891
Ivan Pravdin <72220773+ivanpravdin@users.noreply.github.com>
2022-03-14 17:14:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import collages3d1 from './images/3dcollages1.jpg'
+import collages3d1 from '../images/3dcollages1.jpg'
 
 export default function Home() {
     return (
@@ -68,7 +68,7 @@ export default function Home() {
             </div>
          
          <footer>
-            <p>&nbsp;</p>
+           <br/>
          </footer>
 	    </main>
       
