81f51538065139e45ee9897b4ea2e9fa57e7525e
Ivan Pravdin <72220773+ivanpravdin@users.noreply.github.com>
2022-03-14 18:06:35 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import Image from '/opt/build/repo/pages/images/backgrounxzfd.jpg'
+import Image from '../images/backgrounxzfd.jpg'
 
 export default function Home() {
     return (
