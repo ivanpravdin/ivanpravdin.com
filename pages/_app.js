1e9af3dcf929d7a7eada4e152547f935c9e7623b
IV Pravdin <72220773+ivpravdin@users.noreply.github.com>
2023-02-19 18:39:24 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,4 @@ function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-
-                    window.onload = function() {
-                        Particles.init({
-                            selector: '.background',
-                            connectParticles: true,
-                            color: ["#ffffff"]
-                        })
-                    };
-
 export default Application
