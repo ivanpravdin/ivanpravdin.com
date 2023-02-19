b30de779fc2c30f519cfdd52680fb1f270d4f51c
IV Pravdin <72220773+ivpravdin@users.noreply.github.com>
2023-02-19 18:40:49 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@styles/globals.css'
 
+if (typeof window !== "undefined") {
+  // window.addEventListener("load", () => console.log("LOADED"));
+  document.addEventListener("DOMContentLoaded", () =>
+    console.log("DOMContentLoaded")
+  );
+}
+
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
