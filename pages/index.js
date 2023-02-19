65923108996758dc3f943e6c75de3baa12b34941
IV Pravdin <72220773+ivpravdin@users.noreply.github.com>
2023-02-19 18:02:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,57 +6,50 @@ import Footer from '@components/Footer'
 export default function Home() {
     return (
         <div class="container">
-<head>
-    <title>IV Pravdin</title>
-    <link rel="stylesheet" href="./styles.css"></link>
-</head>
+            <head>
+                <title>IV Pravdin</title>
+                <link rel="stylesheet" href="./styles.css"></link>
+            </head>
 
-<body class="root">
-    <div id="tsparticles"></div>
+            <body class="root">
+                <div id="tsparticles"></div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/tsparticles-engine@2.8.0/tsparticles.engine.min.js"></script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/tsparticles-interaction-particles-links@2.8.0/tsparticles.interaction.particles.links.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/tsparticles-move-base@2.8.0/tsparticles.move.base.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/tsparticles-shape-circle@2.8.0/tsparticles.shape.circle.min.js"></script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/tsparticles-updater-color@2.8.0/tsparticles.updater.color.min.js"></script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/tsparticles-updater-opacity@2.8.0/tsparticles.updater.opacity.min.js"></script>
-    <script
-        src="https://cdn.jsdelivr.net/npm/tsparticles-updater-out-modes@2.8.0/tsparticles.updater.out-modes.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/tsparticles-updater-size@2.8.0/tsparticles.updater.size.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/tsparticles-preset-links@2.8.0/tsparticles.preset.links.min.js"></script>
-    <script type="text/javascript">
-        async () => {
-            await loadLinksPreset(tsParticles)};
-            await tsParticles.load("tsparticles", { preset: "links" });
-        }();
-    </script>
-    <div class="card-client">
-        <p class="name-client"> Hi! My name is IV!
-            <span>I am a CS student at Tufts.
-            </span>
-            <button href="https://drive.google.com/file/d/1xP38WneC1cDi0XH9ZRbZ3PcYdNlaMiOR/view?usp=sharing"> Resume
-            </button>
-        </p>
-        <div class="social-media">
-            <a href="https://www.linkedin.com/in/iv-pravdin/">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                    <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
-                </svg>
-                <span class="tooltip-social">LinkedIn</span>
-            </a>
-            <a href="https://github.com/ivpravdin">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                  </svg>
-                <span class="tooltip-social">GitHub</span>
-            </a>
+
+
+                <script type="module">
+                    window.onload = function() {
+                        Particles.init({
+                            selector: '.background',
+                            connectParticles: true
+                        })
+                    };
+                </script>
+                <div class="card-client">
+                    <p class="name-client"> Hi! My name is IV!
+                        <span>I am a CS student at Tufts.
+                        </span>
+                        <button href="https://drive.google.com/file/d/1xP38WneC1cDi0XH9ZRbZ3PcYdNlaMiOR/view?usp=sharing"> Resume
+                        </button>
+                    </p>
+                    <div class="social-media">
+                        <a href="https://www.linkedin.com/in/iv-pravdin/">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+                            </svg>
+                            <span class="tooltip-social">LinkedIn</span>
+                        </a>
+                        <a href="https://github.com/ivpravdin">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                            </svg>
+                            <span class="tooltip-social">GitHub</span>
+                        </a>
+                    </div>
+                </div>
+                <canvas class="background"></canvas>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.3/particles.min.js"></script>
+            </body>
         </div>
-    </div>
-</body>
-	</div>
-	)
+    )
 }
