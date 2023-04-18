4ec599e509700f739d82e79a91bcf3b5bed9d7ab
IV Pravdin <72220773+ivpravdin@users.noreply.github.com>
2023-04-17 23:37:47 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,16 @@
 import '@styles/globals.css'
 
-useEffect(() => {
-  if(window.innerWidth > 1100){ // pJS_desktop and pJS_mobile = my settings functions
 if (typeof window !== "undefined") {
   // window.addEventListener("load", () => console.log("LOADED"));
   document.addEventListener("DOMContentLoaded", () =>
+    console.log("DOMContentLoaded")
     Particles.init({
                             selector: '.background',
                             connectParticles: true,
                             color: ["#ffffff"]
                         })
   );
-}}
-}, []);
+}
 
 
 function Application({ Component, pageProps }) {
